595879bcf7e22d34fd83c79c6eac0ec7f604376c
Katrin Fritz <katrinfritz@web.de>
2020-04-19 18:35:47 +0200

    Implement firebase and enable to store user data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 import Homepage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 
-import { auth } from './firebase/firebase.utils.js'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js'
 
 class App extends React.Component {
 
@@ -24,8 +24,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+
+          console.log(this.state)
+        })
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
