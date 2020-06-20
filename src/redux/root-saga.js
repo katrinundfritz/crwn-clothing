f0ca4f2f9fca44680b4345abd70558dd0cd6c6ba
Katrin Fritz <katrinfritz@web.de>
2020-06-20 11:24:37 +0200

    Add clear cart funtionality

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,14 +1,13 @@
 import { all, call } from 'redux-saga/effects'
 
-import { fetchCollectionStart } from './shop/shop.sagas'
-import { onGoogleSignInStart, onEmailSigninStart, onCheckUserSession, onSignOutStart } from './user/user.sagas'
+import { shopSagas } from './shop/shop.sagas'
+import { userSagas } from './user/user.sagas'
+import { cartSagas } from './cart/cart.sagas'
 
 export default function* rootSaga() {
   yield all([
-    call(fetchCollectionStart),
-    call(onGoogleSignInStart),
-    call(onEmailSigninStart),
-    call(onCheckUserSession),
-    call(onSignOutStart),
+    call(shopSagas),
+    call(userSagas),
+    call(cartSagas),
   ])
 }
