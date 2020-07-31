3d2683a7cf1fdbaa58cf9cff023897bd7f9dc73a
Katrin Fritz <katrinfritz@web.de>
2020-07-31 15:17:31 +0200

    Adding performance optimizations

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import { GlobalStyle } from './global.styles'
 
-import Homepage from './pages/homepage/homepage.component.jsx'
-import ShopPage from './pages/shop/shop.component.jsx'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import CheckoutPage from './pages/checkout/checkout.component.jsx'
-
 import Header from './components/header/header.component.jsx'
+import Spinner from './components/spinner/spinner.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component'
+
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.actions'
+
+const Homepage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
 
 const App = ({ checkUserSession, currentUser }) => {
 
@@ -25,10 +28,14 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route exact path='/signin' render={() => currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />} />
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path='/' component={Homepage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route exact path='/checkout' component={CheckoutPage} />
+            <Route exact path='/signin' render={() => currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />} />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   )
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -3,9 +3,13 @@ import Directory from '../../components/directory/directory.component.jsx'
 
 import { HomepageContainer } from './homepage.styles'
 
-const Homepage = () =>
-  <HomepageContainer>
+const Homepage = () => {
+  throw Error
+
+  return <HomepageContainer>
     <Directory />
   </HomepageContainer>
+
+}
 
   export default Homepage
